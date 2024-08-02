24dcdac6d776095f46a67c31b7f5dbd4d129859d
Akio <aakocherga@mail.ru>
2024-08-03 01:44:34 +0700

    Added find city weather

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 async function getWeatherData(location="london") {
-  const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=2YNM5FU77T2A8AGV4NNP2Z9T2`, {mode: "cors"})
-  const data = await response.json()
-  return await getDayInfo(data, location)
+  try {
+    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=2YNM5FU77T2A8AGV4NNP2Z9T2`, {mode: "cors"})
+    const data = await response.json()
+    return getDayInfo(data, location)
+  } catch (err) {
+    console.error(err)
+  }
 }
 
-async function getDayInfo(data, location, num=0) {
+function getDayInfo(data, location, num=0) {
   const day = data.days[num]
   const temp = day.temp
   const max = day.tempmax
@@ -24,15 +28,38 @@ function loadHead(name, temp, max, min, preciptype) {
   const headMin = document.querySelector(".head-min")
   const headWeather = document.querySelector(".head-weather")
 
-  const tempCelsium = ((temp - 32) / 1.8).toFixed(1)
+  const tempCelsium = ((temp - 32) / 1.8).toFixed(0) + "°"
+  const maxCelsium = ((max - 32) / 1.8).toFixed(0) + "°"
+  const minCelsium = ((min - 32) / 1.8).toFixed(0) + "°"
   const weather = preciptype.charAt(0).toUpperCase() + preciptype.slice(1)
   const nameTag = name.charAt(0).toUpperCase() + name.slice(1)
 
   headName.innerHTML = nameTag
   headTemp.innerHTML = tempCelsium
-  headMax.innerHTML = max
-  headMin.innerHTML = min
+  headMax.innerHTML = maxCelsium
+  headMin.innerHTML = minCelsium
   headWeather.innerHTML = weather
 }
 
-getWeatherData()
+function eventInput() {
+  const input = document.querySelector(".input-find-city")
+  const btn = document.querySelector(".search-city")
+
+  btn.addEventListener("click", () => {
+    console.log("active")
+    if (input.value !== "") {
+      console.log("active-find")
+      getWeatherData(input.value)
+      input.value = ""
+    }
+  })
+}
+
+(async () => {
+  try {
+    await getWeatherData()
+    eventInput()
+  } catch (err) {
+    console.error(err)
+  }
+})()
